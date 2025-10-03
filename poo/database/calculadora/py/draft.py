058e8045022eded7a831ccaabf9951b45622d97d
will-almeidaof <willyamabr@gmail.com>
2025-10-03 00:17:41 +0000

    fazendo a atividade da calculadora, dnv

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -1,9 +1,81 @@
-class Calculadora:
-    def __init__(self,batterymax: int):
-        self.display: float = 0
-        self.battery: int = 0
-        self.batteryMax: int = batterymax
+# |    Cauculator    |
+# |------------------|
+# | battery: int     |
+# | maxBattery: int  |
+# | display: float   |
+# --------------------
 
-    def __str__(self) -> str:
-        return f"{self.display}:{self.battery}:{self.batteryMax}"
+
+
+# Contruindo classe Calculator
+
+class Calculator:
+  def _init_(self, maxBattery: int):
+    self.battery:    int   = 0;
+    self.maxBattery: int   = maxBattery;
+    self.display:    float = 0.00;
+
+  def _str_(self) -> str:
+    return f"display = {self.display:.2f}, battery = {self.battery}";
+
+  def charge (self, value: int):
+    self.battery += value;
+    if self.battery > self.maxBattery:
+      self.battery = self.maxBattery;
+
+  def sum(self, a: float, b: float):
+    if self.battery == 0:
+      print("fail: bateria insuficiente");
     
+    else:
+      self.display = a + b;
+      self.battery -= 1;
+
+  def division(self, num: float, den:float):
+    if self.battery == 0:
+      print("fail: bateria insuficiente");
+
+    elif den == 0:
+      print("fail: divisao por zero");
+      self.battery -= 1;
+
+    else:
+      self.display = num / den;
+      self.battery -= 1;
+
+# Contrindo função main()
+
+def main():
+  calculator: Calculator = Calculator();
+
+  while True:
+    line: str = input();
+    args: list[str] = line.split(" ");
+    
+    print(f"${line}");
+
+    match args[0]:
+      case "init":
+        calculator.maxBattery = int(args[1]);
+        calculator.battery = 0;
+        calculator.display = 0;
+      
+      case "show":
+        print(calculator);
+
+      case "charge":
+        calculator.charge(int(args[1]));
+
+      case "sum":
+        calculator.sum(int(args[1]), int(args[2]));
+
+      case "div":
+        calculator.division(int(args[1]), int(args[2]));
+      
+      case "end":
+        break;
+
+      case _:
+        print("error: comando não encontrado");
+
+main();
